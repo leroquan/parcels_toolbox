f8fc85c9c69db1a6e459b226e817455473b8daef
Leroquais <leroquan@eawag.wroot.emp-eaw.ch@KBALL-LAKEN-L.eawag.wroot.emp-eaw.ch>
2025-04-10 17:00:25 +0200

    removed venv

--- venv/lib/python3.12/site-packages/pymbolic/maxima.py
+++
@@ -1,6 +0,0 @@
-from pymbolic.interop.maxima import *  # noqa
-
-from warnings import warn
-
-warn("pymbolic.maxima is deprecated. Use pymbolic.interop.maxima instead",
-     DeprecationWarning, stacklevel=1)
--- venv/lib/python3.12/site-packages/pymbolic/sympy_interface.py
+++
@@ -1,6 +0,0 @@
-from pymbolic.interop.sympy import *  # noqa
-
-from warnings import warn
-
-warn("pymbolic.sympy_interface is deprecated. Use pymbolic.interop.sympy instead",
-     DeprecationWarning, stacklevel=1)
