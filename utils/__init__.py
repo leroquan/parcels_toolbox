8699e3f982ad881803427f2fda9d61609dba9898
leroquan <anne.leroquais@eawag.ch>
2026-02-03 13:59:43 +0100

    run_parcels and map_particles used for the 2026 01 28 field trip (Madeline)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-from .coordinates_conversion_utils import (translate_grid_to_origin,
-                                          get_grid_angle,
-                                          rotate_grid)
+from .coordinates_conversion_utils import (translate_grid,
+                                           get_grid_angle,
+                                           rotate_grid,
+                                           convert_mitgcm_to_epsg_coord)
